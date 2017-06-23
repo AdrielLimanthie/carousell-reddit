ea3eb46d494945b8515944e70d7eb26d2e72dc31
NikodemusLimanthie <nikodemus.limanthie@tokopedia.com>
2017-06-24 01:06:57 +0700

    Move header to a component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { IndexLink } from 'react-router'
+
+import './Header.scss'
+
+class Header extends Component {
+  render () {
+    return (
+      <header className='header'>
+        <IndexLink to='/' activeClassName='header__link'>
+          <h1>Carousell Reddit</h1>
+        </IndexLink>
+      </header>
+    )
+  }
+}
+
+export default Header
