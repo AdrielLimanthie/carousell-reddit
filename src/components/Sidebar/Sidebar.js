c4d73b9a0fff51e4ed0d074704dc92abf4c82e56
AdrielLimanthie <a.limanthie@yahoo.com>
2017-06-24 20:21:00 +0700

    Limit topic name length to 255

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -47,6 +47,7 @@ class Sidebar extends Component {
           <h6>Name of the Topic</h6>
           <input type='text'
             className='input mb-15'
+            maxLength={255}
             value={this.state.topicName}
             onChange={e => { this.handleStateChange('topicName', e.target.value) }}
             onKeyPress={e => e.key === 'Enter' && this.handleSubmitTopic(this.state.topicName)}
